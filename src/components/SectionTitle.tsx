c6fa516e06590ef18d7a1fde7ef95607890b573e
mdnuruzzamannirob <nuruzzamanmd2002@gmail.com>
2025-09-11 23:46:31 +0600

    update tech stack section

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -12,7 +12,7 @@ const SectionTitle = ({ title, description, className }: ITitleProps) => {
       className={cn('flex flex-col items-center justify-center space-y-5 text-center', className)}
     >
       <h1 className="text-4xl font-bold capitalize">{title}</h1>
-      <p className="">{description}</p>
+      <p className="mx-auto max-w-xl text-gray-400">{description}</p>
     </div>
   );
 };
