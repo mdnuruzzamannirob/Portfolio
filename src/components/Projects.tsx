40d5f85948e5906ee3a75a583990c13f8d96ea33
mdnuruzzamannirob <nuruzzamanmd2002@gmail.com>
2025-09-12 17:47:38 +0600

    add images of projects section. update projects section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,8 +4,10 @@ import { useState } from 'react';
 import { projects } from '@/constants/projects';
 import SectionTitle from './SectionTitle';
 import Link from 'next/link';
-import { MdArrowForward, MdArrowOutward } from 'react-icons/md';
+import { MdArrowOutward } from 'react-icons/md';
 import { useSmoothScroll } from '@/hooks/useSmoothScroll';
+import { cn } from '@/utils/cn';
+import Image from 'next/image';
 
 const Projects = () => {
   const smoothScroll = useSmoothScroll();
@@ -33,52 +35,75 @@ const Projects = () => {
 
       <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
         {projects?.slice(0, visibleCount)?.map((project, index) => (
-          <div className="space-y-4 rounded-xl border border-white/5 bg-white/[2%] p-5" key={index}>
-            <div className="h-60 w-full rounded-xl bg-white/10">
-              {/* <Image alt="" src={project?.image[0]} width={500} height={500} className="size-full object-cover rounded-xl" /> */}
-            </div>
+          <div
+            className="flex flex-col rounded-xl border border-white/5 bg-white/[2%] p-3"
+            key={index}
+          >
+            <div className="flex-1 space-y-4 pb-3">
+              <div className="h-60 w-full rounded-xl bg-white/10">
+                <Image
+                  alt=""
+                  src={project?.image}
+                  width={500}
+                  height={500}
+                  className="size-full rounded-xl"
+                />
+              </div>
 
-            <div className="space-y-1">
-              <h2 className="text-lg font-medium">
-                {project?.title}{' '}
-                <span className="ml-1 text-xs text-white/50">({project?.projectType})</span>
-              </h2>
-              <p className="text-sm text-white/70">{project?.description}</p>
-            </div>
-
-            <div className="flex flex-col gap-1">
-              <p className="text-sm">Tech stack:</p>
-              <div className="flex flex-wrap items-center gap-1">
-                {project?.techStack?.map((stack, i) => (
-                  <div
-                    className="flex items-center gap-2 rounded-md border border-white/10 bg-white/5 p-1 px-1.5 text-xs"
-                    key={i}
+              <div className="space-y-1">
+                <h2 className="text-lg font-medium">
+                  {project?.title}{' '}
+                  <span
+                    className={cn(
+                      'ml-1 text-xs whitespace-nowrap',
+                      project?.projectType.toLowerCase().startsWith('t')
+                        ? 'text-tertiary'
+                        : 'text-secondary',
+                    )}
                   >
-                    <p>{stack?.icon}</p>
-                    <h3>{stack?.name}</h3>
-                  </div>
-                ))}
+                    ({project?.projectType})
+                  </span>
+                </h2>
+                <p className="text-sm text-white/70">{project?.description}</p>
+              </div>
+
+              <div className="flex flex-col gap-1">
+                <p className="text-sm">Tech stack:</p>
+                <div className="flex flex-wrap items-center gap-1">
+                  {project?.techStack?.map((stack, i) => (
+                    <div
+                      className="flex items-center gap-2 rounded-md border border-white/10 bg-white/5 p-1 px-1.5 text-xs"
+                      key={i}
+                    >
+                      <p>{stack?.icon}</p>
+                      <h3>{stack?.name}</h3>
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
 
-            <div className="flex items-center justify-end gap-2 text-xs">
+            <div className="flex items-center justify-end gap-2 border-t border-white/5 pt-3 text-xs">
               <Link
                 href={project?.github?.frontend}
-                className="group border-primary/30 text-primary hover:bg-primary/20 flex items-center gap-1 rounded-md border px-2 py-1 transition"
+                className="group border-primary/30 text-primary hover:bg-primary/20 flex items-center gap-1 rounded-md border px-3 py-2 transition"
               >
                 GitHub Client{' '}
                 <MdArrowOutward className="rotate-45 transition group-hover:rotate-0" />
               </Link>
-              <Link
-                href={project?.github?.backend}
-                className="group border-primary/30 text-primary hover:bg-primary/20 flex items-center gap-1 rounded-md border px-2 py-1 transition"
-              >
-                GitHub Server{' '}
-                <MdArrowOutward className="rotate-45 transition group-hover:rotate-0" />
-              </Link>
+              {project?.github?.backend && (
+                <Link
+                  href={project?.github?.backend}
+                  className="group border-primary/30 text-primary hover:bg-primary/20 flex items-center gap-1 rounded-md border px-3 py-2 transition"
+                >
+                  GitHub Server{' '}
+                  <MdArrowOutward className="rotate-45 transition group-hover:rotate-0" />
+                </Link>
+              )}
+
               <Link
                 href={project?.liveUrl}
-                className="group border-primary/30 text-primary hover:bg-primary/20 flex items-center gap-1 rounded-md border px-2 py-1 transition"
+                className="group border-primary/30 text-primary hover:bg-primary/20 flex items-center gap-1 rounded-md border px-3 py-2 transition"
               >
                 Live Site <MdArrowOutward className="rotate-45 transition group-hover:rotate-0" />
               </Link>
@@ -88,7 +113,7 @@ const Projects = () => {
       </div>
 
       {/* Load More / Show Less button */}
-      {projects.length > initialCount && (
+      {projects?.length > initialCount && (
         <div className="flex justify-center">
           <button
             onClick={handleShowMoreOrLess}
